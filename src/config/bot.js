417e1fdc354a207f62bfd5f43d852bc6fceb2a7b
ghostsweap <ghostsweap@gmail.com>
2026-05-27 01:57:03 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made By Piggycatfofo",
         // Activity type number (0 = Playing).
         type: 0, 
       },
@@ -88,7 +88,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "#6cb1df", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
